500c6c288715c9cc909899511fcd4fac617aa6c0
Jmnote <opcore@gmail.com>
2021-09-18 16:25:14 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 curl -V > /dev/null && echo curl OK || apt-get install -y curl
 
 grep '^alias ll' ~/.bashrc > /dev/null || echo "alias ll='ls -alF --color'" >> ~/.bashrc
+grep '^alias ls' ~/.bashrc > /dev/null || echo "alias ls='ls --color'" >> ~/.bashrc
 grep '^PS1' ~/.bashrc > /dev/null || echo 'PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\\$ "' >> ~/.bashrc
