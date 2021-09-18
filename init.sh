a5bbd643806b9cf81f0df272fc8759a3289eba3f
Jmnote <opcore@gmail.com>
2021-09-18 16:23:19 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 curl -V > /dev/null && echo curl OK || apt-get install -y curl
+
 grep '^alias ll' ~/.bashrc > /dev/null || echo "alias ll='ls -alF --color'" >> ~/.bashrc
+grep '^PS1' ~/.bashrc > /dev/null || echo 'PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\\$ "' >> ~/.bashrc
