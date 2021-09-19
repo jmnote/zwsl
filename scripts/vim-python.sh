4c492af1f40347c38176dc2538333e8ba6192b2c
Jmnote <opcore@gmail.com>
2021-09-19 13:07:56 +0900

    Update vim-python.sh

--- scripts/vim-python.sh
+++ scripts/vim-python.sh
@@ -11,22 +11,16 @@ cat <<EOF > ~/.vimrc
 set nocompatible
 filetype off
 set rtp+=~/.vim/bundle/Vundle.vim
-
 """"
 call vundle#begin()
-
 Plugin 'VundleVim/Vundle.vim'
 Plugin 'davidhalter/jedi-vim'
 autocmd FileType python setlocal completeopt-=preview
-
 Plugin 'hynek/vim-python-pep8-indent'
 filetype plugin indent on
-
 Plugin 'nvie/vim-flake8'
-
 call vundle#end()
 """"
-
 if has("syntax")
 syntax on
 endif
@@ -35,10 +29,7 @@ set encoding=utf-8
 set number
 set tabstop=4
 set cursorline
-
 set laststatus=2
 set statusline=\ %<%l:%v\ [%P]%=%a\ %h%m%r\ %F\
-
-""""
 au FileType python map <f2> : !python3 %
 EOF
